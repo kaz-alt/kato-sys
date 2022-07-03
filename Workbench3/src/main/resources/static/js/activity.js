5039f2fe612a3dc9757acc49126a1760128e5e3a
Kazuya Kato <kazuyakato1123@gmail.com>
2022-07-03 14:31:32 +0900

    #activity added validation for create method

--- Workbench3/src/main/resources/static/js/activity.js
+++ Workbench3/src/main/resources/static/js/activity.js
@@ -65,17 +65,19 @@ $(function(){
 	$('.activity-edit-button').on('click', function(event){
 		event.preventDefault();
 
-    let paramUrl = $(this).data("href") + '?id=' + $(this).data("id");
+    let paramUrl = $(this).data("href");
+    let id = $(this).data("id");
 
     // fragment(html)取得
     $.ajax({
       type : "GET",
       url : paramUrl,
+      data : {id : id},
       dataType : "html"
     }).done(function(data){
       $('#activity-edit-form').html(data);
       $('.datepicker').datepicker(window.main.datepicker());
-      window.main.createClientOptionWithId($('#activity-edit-form select[name="clientId"]'));
+      window.main.createEmployeeOptionWithId($('#activity-edit-form select[name="staffId"]'));
     }).fail(function(){
       alert("データ取得に失敗しました");
     })
@@ -87,21 +89,21 @@ $(function(){
 
 		if(path.indexOf('detail') > 0){
 			$.ajax({
-	 			type : $form.attr('method'),
-　　　　			url : $form.attr('action'),
-				data: $form.serialize(),
-　　			}).done(function(){
-				alert('データを更新しました。');
-				location.reload();
+        type : $form.attr('method'),
+        url : $form.attr('action'),
+				data: $form.serialize()
+      }).done(function(){
+        alert('データを更新しました。');
+        location.reload();
 			}).fail(function(){
 				alert("データの更新に失敗しました");
 			})
 		} else{
-			$('#product-edit-form').submit();
+			$('#activity-edit-form').submit();
 		}
 	})
 
-	$('.product-del-button').click(function(){
+	$('.activity-del-button').click(function(){
 
 		let id = $(this).data('id');
 		let url = $(this).data('href');
@@ -113,119 +115,86 @@ $(function(){
 		if(confirm){
 
 			let $form = $('<form/>', {action: url, method: 'POST'})
-	  				.append($('<input/>', {type: 'hidden', name: 'productId', value: id}))
-	  				.append($('<input/>', {type: 'hidden', name: '_csrf', value: _csrf}))
-	  				.appendTo(document.body);
+        .append($('<input/>', {type: 'hidden', name: 'id', value: id}))
+        .append($('<input/>', {type: 'hidden', name: '_csrf', value: _csrf}))
+        .appendTo(document.body);
 
 			if(path.indexOf('detail') > 0){
 				$.ajax({
-		 			type : 'POST',
+          type : 'POST',
 					url : url,
 					data: $form.serialize()
-	　　			}).done(function(){
+        }).done(function(){
 					alert('データを削除しました。');
 					location.reload();
 				}).fail(function(){
 					alert("データの削除に失敗しました");
 				})
-
-		    } else{
-
-	  				$form.submit();
-
-		    }
+      } else{
+        $form.submit();
+      }
 		}
 	})
 
 	$.extend($.validator.messages, {
-   	 	required: '*入力必須です',
-		date: "*正しい日付形式で入力してください"
-  	});
+    required: '*入力必須です',
+		date: "*正しい日付形式で入力してください",
+    maxlength: "*2,000文字以内で入力してください"
+  });
 
 	//追加ルールの定義
-  	let methods = {
-		dateFormat: function(value, element){
-      	return this.optional(element) || /^\d{4}-\d{2}-\d{2}$/.test(value);
-    	}
-  	};
+  let methods = {
+    dateFormat: function(value, element){
+      return this.optional(element) || /^\d{4}-\d{2}-\d{2}$/.test(value);
+    }
+  };
 
 	//メソッドの追加
-  	$.each(methods, function(key) {
-    	$.validator.addMethod(key, this);
-  	});
+  $.each(methods, function(key) {
+    $.validator.addMethod(key, this);
+  });
 
-  	//入力項目の検証ルール定義
-  	let rules = {
-    	productName: {required: true},
-		clientId: {required: true, min: 1},
-		productQuantity: {required: true, min: 1, number: true},
-    	productSales: {required: true, min: 1, number: true},
-		purchasedDate: {required: true, date: true, dateFormat: true},
-		startProductSales: {min: 0, number: true},
-		endProductSales: {min: 0, number: true},
-		startPurchasedDate: {date: true, dateFormat: true},
-		endPurchasedDate: {date: true, dateFormat: true}
-  	};
+  //入力項目の検証ルール定義
+  let rules = {
+    staffId: {required: true, min: 1},
+    activityDate: {required: true, date: true, dateFormat: true},
+    content: {required: true, maxlength: 2000},
+    startActivityDate: {date: true, dateFormat: true},
+    endActivityDate: {date: true, dateFormat: true}
+  };
 
-  	//入力項目ごとのエラーメッセージ定義
-  	let messages = {
-    	productName: {
-      		required: "*製品・サービス名を入力してください"
-    	},
-    	clientId: {
-      		required: "*顧客を選択してください",
-			min: "*顧客を選択してください"
-    	},
-		productQuantity: {
-			required: "*個数を入力してください",
-			min: "*１以上で入力してください",
-			number: "*数値を入力してください"
-		},
-    	productSales: {
-      		required: "*金額を選択してください",
-			min: "*１以上で入力してください",
-			number: "*数値を入力してください"
-    	},
-		interviewDate: {
-      		required: "*受注日を選択してください",
-			dateFormat: "*正しい日付形式で入力してください"
-    	},
-        startProductSales: {
-	        min: "*0以上で入力してください",
-            number: "*数値を入力してください"
-        },
-        endProductSales: {
-	        min: "*0以上で入力してください",
-            number: "*数値を入力してください"
-        }
-  	};
+  //入力項目ごとのエラーメッセージ定義
+  let messages = {
+    clientsstaffIdtaffIdId: {required: "*社員を選択してください",min: "*社員を選択してください"},
+    activityDate: {required: "*活動日を選択してください", dateFormat: "*正しい日付形式で入力してください"},
+  };
 
-	validate($("#product-form"));
+	validate($("#activity-form"));
 
-	validate($("#product-edit-form"));
+	validate($("#activity-edit-form"));
 
-	validate($('#productSearchForm'));
+	validate($('#activitySearchForm'));
 
 	function validate($form){
 
 		$form.validate({
-      		rules: rules,
-      		messages: messages,
+      rules: rules,
+      messages: messages,
 
-      		//エラーメッセージ出力箇所調整
-      		errorPlacement: function(error, element){
- 				error.addClass('text-danger');
-				if (element.is('select')) {
-          			error.appendTo(element.parent());
-        		}else {
-					if(element.hasClass('group') || element.hasClass('search')){
-						error.appendTo(element.parent().parent());
-					} else {
-						error.insertAfter(element);
-					}
-        		}
-      		}
-    	});
+      //エラーメッセージ出力箇所調整
+      errorPlacement: function(error, element){
+        error.addClass('text-danger');
+        if (element.is('select')) {
+          error.appendTo(element.parent());
+        }else {
+          if(element.hasClass('group') || element.hasClass('search')){
+            error.appendTo(element.parent().parent());
+          } else {
+          error.insertAfter(element);
+          }
+        }
+      }
+    });
 	}
 
 })
