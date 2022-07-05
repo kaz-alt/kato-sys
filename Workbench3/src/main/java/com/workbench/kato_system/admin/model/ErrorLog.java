5be7c30cff04b8c4eb0f5f9c5dd7ca82bfcb48f7
Kazuya Kato <kazuyakato1123@gmail.com>
2022-07-05 22:09:12 +0900

    #global_error save error path

---
+++ Workbench3/src/main/java/com/workbench/kato_system/admin/model/ErrorLog.java
@@ -0,0 +1,52 @@
+package com.workbench.kato_system.admin.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * エラー
+ * @author katoukazuya
+ *
+ */
+@Entity
+@Table(name = "error_log")
+@Getter
+@Setter
+public class ErrorLog implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	/* エラーパス */
+	@Column(name = "error_path")
+	private String errorPath;
+
+	/* 作成者 */
+	@Column(name = "created_by")
+	private String createdBy;
+
+	/* 作成日時 */
+	@Column(name = "created_date", nullable = false, updatable = false)
+	private LocalDateTime createdDate;
+
+  public ErrorLog(String errorPath, String createdBy, LocalDateTime createdDateTime) {
+    this.errorPath = errorPath;
+    this.createdBy = createdBy;
+    this.createdDate = createdDateTime;
+  }
+}
